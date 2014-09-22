57b54fd9fdc500e04965c3809341b89a7db0f3e7
dergachev.d <gnomdan@yandex.ru>
2014-09-22 22:43:42 +0400

    javascript at most instead of jquery, savedlist started

--- game-of-life/main.js
+++ game-of-life/main.js
@@ -5,21 +5,18 @@ $(function () {
     Game = function (x, y, speed) {
         this.sizeX = +x;
         this.sizeY = +y;
-        this.state = new Array(this.sizeX);
+        this.state = [];
         this.isTableSet = false;
-        this.objArr = new Array(this.sizeX);
-        this.neght = new Array(this.sizeX);
+        this.objArr = [];
+        this.neght = [];
         this.isGameGoing = false;
-
         for (var i = 0; i < this.sizeX; i++) {
-            this.state[i] = new Array(this.sizeY);
-            this.neght[i] = new Array(this.sizeY);
-
-            this.objArr[i] = new Array(this.sizeY);
+            this.state[i] = [];
+            this.neght[i] = [];
+            this.objArr[i] = [];
             for (var j = 0; j < this.sizeY; j++) {
                 this.state[i][j] = false;
                 this.neght[i][j] = 1;
-
             }
         }
         var game = this;
@@ -29,30 +26,34 @@ $(function () {
             }
         },speed);
     };
-    Game.prototype.wrapper = $('#game');
+    Game.prototype.wrapper = document.getElementById('game');
     Game.prototype.createTable = function () {
-        var game = this, i, j;
+        var game = this,i, j;
+        game.removeTable();
         for (i = 0; i < game.sizeX; i++) {
-            this.wrapper.append('<div class="r"></div>');
-        }
-        var rowCount = 0;
-
-        $('.r').each(function () {
+            var row = document.createElement('div');
+            row.className = 'r';
+            this.wrapper.appendChild(row);
             for (j = 0; j < game.sizeY; j++) {
-                $(this).append('<div class="c" id="c_' + rowCount + '_' + j + '"></div>');
-            }
-            rowCount++;
-        });
-        for (i = 0; i < game.sizeX; i++) {
-            for (j = 0; j < game.sizeY; j++) {
-                game.objArr[i][j] = $('#c_' + i + '_' + j);
+                var cell = document.createElement('div');
+                cell.className = 'c';
+                cell.id = 'c_'+i+'_'+j;
+                row.appendChild(cell);
+                this.objArr[i][j] = cell;
+                cell.onclick = function(){
+                    var arr = this.id.split("_");
+                    var x = arr[1];
+                    var y = arr[2];
+                    if(game.state[x][y]){
+                        this.classList.remove('a');
+                        game.state[x][y] = false;
+                    } else {
+                        this.classList.add('a');
+                        game.state[x][y] = true;
+                    }
+                };
             }
         }
-        $('.c').click(function () {
-            $(this).toggleClass('a');
-            var arr = $(this).attr('id').split("_");
-            game.state[arr[1]][arr[2]] = (game.state[arr[1]][arr[2]] ? false : true);
-        });
         this.isTableSet = true;
     };
     Game.prototype.nextState = function () {
@@ -90,13 +91,32 @@ $(function () {
                 if (next[x][y] != this.state[x][y]) {
                     this.isGameGoing = true;
                     if (next[x][y]) {
-                        this.objArr[x][y].addClass('a');
+                        this.objArr[x][y].classList.add('a');
                     } else {
-                        this.objArr[x][y].removeClass('a');
+                        this.objArr[x][y].classList.remove('a');
                     }
                 }
             }
         }
+    };
+    Game.prototype.getList = function(){
+        var game = this;
+        $.get('list.txt',function(data){
+            data = data.substring(0, data.length - 1);
+            var arr = data.split(',');
+            game.wrapper.append('<div class="list"'); /// stoped here
+            for(var elem in arr){
+
+            }
+        });
+    };
+    Game.prototype.changeSize = function(x,y){
+        this.sizeX = x;
+        this.sizeY = y;
+    };
+    Game.prototype.removeTable = function(){
+        this.wrapper.innerHTML = '';
+        this.isTableSet = false;
     };
     Game.prototype.random = function () {
         if (!this.isTableSet) this.createTable();
@@ -104,7 +124,7 @@ $(function () {
             for (var j = 0; j < this.sizeY; j++) {
                 if (Math.random() > .7) {
                     this.state[i][j] = true;
-                    this.objArr[i][j].addClass('a');
+                    this.objArr[i][j].classList.add('a');
                 }
             }
         }
@@ -133,41 +153,43 @@ $(function () {
             $.post('write.php', {name: name, arr: jsonString});
         }
     };
+    Game.prototype.updateTable = function(x,y){
+        game.changeSize(x,y);
+        game.createTable();
+    };
     Game.prototype.get = function(){
-        if (!this.isTableSet) this.createTable();
+        if (this.isTableSet) this.removeTable();
         var name = $('#name').val();
         var game = this;
         $.post('read.php',{name: name},function(data){
             var state=JSON.parse(data);
+            game.updateTable(state.length,state[0].length);
             game.render(state);
             game.state = state;
-            game.isGameGoing = false;
+
+            game.stop();
         });
 
     };
+    Game.prototype.addController = function(func,text){
+        var game = this;
+        var btn = document.createElement('button');
+        btn.innerHTML = text;
+        btn.onclick = function(){
+            game[func]();
+        };
+        document.body.appendChild(btn);
+    };
 
-    game = new Game(100, 100, 0);
+    game = new Game(250, 250, 0);
 
-    $('#makeTable').click(function () {
-        game.createTable();
-    });
-    $('#nextstate').click(function () {
-        game.start();
-    });
-    $('#random').click(function () {
-        game.random();
-    });
-    $('#stop').click(function(){
-        game.stop();
-    });
-    $('#clear').click(function(){
-        game.clear();
-    });
-    $('#send').click(function(){
-        game.send();
-    });
-    $('#get').click(function(){
-        game.get();
-    })
+    game.addController("createTable","Создать поле");
+    game.addController("random","Заполнить случайно");
+    game.addController("start","Старт");
+    game.addController("stop","Стоп");
+    game.addController("clear","Очистить поле");
+    game.addController("send","Сохранить состояние");
+    game.addController("get","Загрузить по названию файла");
+
 
 });
